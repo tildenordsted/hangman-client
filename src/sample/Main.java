60d3521ae299e1407262cb6f84452750e86ca627
tildenordsted <56248200+tildenordsted@users.noreply.github.com>
2020-10-21 11:08:42 +0200

    Button response
    added if\else statement to check if text field is empty

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,15 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
@@ -19,7 +22,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hangman Login");
 
         //Velkomstlabel
-        Label welcomeLabel = new Label("Velkommen til Hangman, opret bruger her:");
+        Label welcomeLabel = new Label("Velkommen til Hangman! Opret bruger her:");
 
         //Tekstboks
         TextField usernameField = new TextField("Indtast brugernavn");
@@ -27,14 +30,35 @@ public class Main extends Application {
         //Loginbutton
         Button loginButton = new Button("Opret");
 
-        //Opretter tile pane objekt (r = root)
-        TilePane r = new TilePane();
+        //Oprettelseslabel
+        Label loginCheck = new Label();
+
+        //Omrokerer indholdet
+        welcomeLabel.relocate(150,125);
+        usernameField.relocate(150,150);
+        loginButton.relocate(320,150);
+        loginCheck.relocate(150,200);
+
+        //Respons på knappen
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                if (usernameField.getText() == null || usernameField.getText().trim().isEmpty()) {
+                    loginCheck.setText("nono");
+                } else {
+                    loginCheck.setText("yesyes");
+                }
+            }
+        });
+
+
+        //Opretter pane objekt (r = root)
+        Pane r = new Pane();
 
         //Tilføjer label, username og loginbutton til tile
         r.getChildren().add(welcomeLabel);
         r.getChildren().add(usernameField);
         r.getChildren().add(loginButton);
-
+        r.getChildren().add(loginCheck);
 
 
         primaryStage.setScene(new Scene(r, 500, 500));
